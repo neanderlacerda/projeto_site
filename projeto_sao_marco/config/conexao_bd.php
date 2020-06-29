7b04c5d053c1aae8f780d355741217ebc3c297e7
Neander <31941817+neanderlacerda@users.noreply.github.com>
2020-06-29 16:11:28 -0300

    Subindo projeto com as pastas organizadas
    Estava criando o projeto em outro repositório, porém estava todo bagunçado e quando testei em outro computador ele não funcionou. Criei este repositório com tudo organizado!

---
+++ projeto_sao_marco/config/conexao_bd.php
@@ -0,0 +1,15 @@
+<?php
+
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
+    $link = mysqli_connect("localhost", "root", "", "saomarco");
+
+    if (!$link) {
+        echo "Erro: Não foi possível realizar uma conexão com o MySQL." . "<br>";
+        echo "Número do erro: " . mysqli_connect_errno() . "<br>";
+        echo "Mensagem de erro: " . mysqli_connect_error() . "<br>";
+        exit;
+    }
+?>
---
+++ projeto_sao_marco/paginas/inicio.php
@@ -0,0 +1,12 @@
+<div class="row">
+
+    <div class="col">
+
+        <h1>Início</h1>
+
+        <p>
+            Utilize este site para abrir chamados para técnicos da informática.
+        </p>
+    </div>
+
+</div>
